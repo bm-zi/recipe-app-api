defe312e574802b78c367cc9d6e036fb066ac4c5
bmzi <bmzi@lab.localdomain>
2022-10-13 11:49:37 -0400

    build recipe api

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,12 +1,9 @@
-
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
-
 from django.contrib import admin
 from django.urls import path, include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
@@ -16,5 +13,5 @@ urlpatterns = [
         name='api-docs',
     ),
     path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls')),
 ]
-
